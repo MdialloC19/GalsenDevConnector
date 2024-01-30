bfdcd3eba039621724ac158ce8a4ed68fb2faf04
MdialloC19 <mdiallo3211@gmail.com>
2024-01-30 20:00:54 +0000

    add:user authentifcation and logind route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const auth = require("../../middleware/auth");
 const router = express.Router();
 const User = require("../../models/User");
-
+const { check, validationResult } = require("express-validator");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 /**
  * @desc Route to get all auth.
  * @route GET api/auth
@@ -20,5 +23,81 @@ router.get("/", auth, async (req, res) => {
     });
   }
 });
+
+/**
+ * @desc Authenticate user & get token
+ * @route POST api/auth
+ * @access Public
+ */
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "passsword is required ").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    console.log(email, password);
+
+    try {
+      let user = await User.findOne({ email });
+      console.log(user);
+      //see if user exists
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid Credentials user" }],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid Credentials" }],
+        });
+      }
+      // Encrypt password
+
+      // const salt = await bcrypt.genSalt(10);
+
+      // user.password = await bcrypt.hash(password, salt);
+
+      // await user.save();
+
+      // Return jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "5 days" },
+        (err, token) => {
+          if (err) throw err;
+          return res.status(201).json({
+            token,
+            sucess: true,
+            message: "Authentificated",
+          });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        sucess: false,
+        error: error.message,
+      });
+    }
+  }
+);
 
 module.exports = router;
